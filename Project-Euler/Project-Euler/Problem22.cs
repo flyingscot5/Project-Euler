be843b3b5babac9c2b1fb4cd7dbf95c6be169513
flyingscot5 <>
2018-02-12 16:56:30 +0000

    added solution for problem 22

--- Project-Euler/Project-Euler/Problem22.cs
+++ Project-Euler/Project-Euler/Problem22.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Project_Euler
 {
@@ -10,10 +8,32 @@ namespace Project_Euler
     {
         public void ReturnResult()
         {
+            var listOfNames = File.ReadAllLines("../../Resources/Problem22/names.txt").First();
 
+            var arrayOfNames = listOfNames.Split(',');
 
+            var sortedNames = arrayOfNames.OrderBy(n => n);
 
-            Console.WriteLine("Problem 22:\n ");
+            var index = 0;
+
+            double finaleTotal = 0;
+
+            foreach(var name in sortedNames)
+            {
+                var total = 0;
+
+                index++;
+                var charArrayOfNames = name.ToCharArray();
+                foreach(var eachChar in charArrayOfNames)
+                {
+                    var currentNum = Convert.ToUInt16(eachChar);
+                    total += ( currentNum - 64 );
+                }
+                total *= index;
+                finaleTotal += total;
+            }
+
+            Console.WriteLine("Problem 22:\nThe total of all the name scores " + finaleTotal);
         }
     }
 }
