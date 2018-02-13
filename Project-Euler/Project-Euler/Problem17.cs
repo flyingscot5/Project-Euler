72f7ae5f89109df3a7b819d5ebb6317ac0b3c6d6
flyingscot5 <>
2018-02-13 14:05:07 +0000

    added solution for 17
    this is complete rubbish plan to redo

--- Project-Euler/Project-Euler/Problem17.cs
+++ Project-Euler/Project-Euler/Problem17.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Project_Euler
 {
@@ -6,54 +7,109 @@ namespace Project_Euler
     {
         public void ReturnResult()
         {
-            int counter = 0;
-
-            int singlesTotal = 0;
-
-            int patternLessTotal = 0;
-
-            int doublesTotal = 0;
-
-            //1-9
-            var singles = "3,3,5,4,4,3,5,5,4";
-            //10-19
-            var patternLess = "3,6,6,8,8,7,7,9,8,8";
-            //20-90 in tens
-            var doubles = "6,6,5,5,5,7,6,6";
-
-            var arrayOfSingles = singles.Split(',');
-            var arrayOfPatternLess = patternLess.Split(',');
-            var arrayOfDoubles = doubles.Split(',');
-
-            for(int j = 0; j < 9; j++)
+            var basicNums = new List<int>()
             {
-                //sum singles
-                for(int i = 0; i < 9; i++)
-                {
-                    singlesTotal += Int32.Parse(arrayOfSingles[i]);
-                }
+                "one".Length,
+                "two".Length,
+                "three".Length,
+                "four".Length,
+                "five".Length,
+                "six".Length,
+                "seven".Length,
+                "eight".Length,
+                "nine".Length
+            };
+
+            var nonBaiscNums = new List<int>()
+            {
+                "ten".Length,
+                "eleven".Length,
+                "twelve".Length,
+                "thirteen".Length,
+                "fourteen".Length,
+                "fifteen".Length,
+                "sixteen".Length,
+                "seventeen".Length,
+                "eighteen".Length,
+                "nineteen".Length
+            };
+
+            var tenthNums = new List<int>()
+            {
+                "twenty".Length,
+                "thirty".Length,
+                "forty".Length,
+                "fifty".Length,
+                "sixty".Length,
+                "seventy".Length,
+                "eighty".Length,
+                "ninety".Length
+            };
+
+            var and = "and".Length;
+            var hundred = "hundred".Length;
+            var onethousand = "onethousand".Length;
 
 
-                //sum of patternless
-                for(int i = 0; i < 10; i++)
-                {
-                    patternLessTotal += Int32.Parse(arrayOfPatternLess[i]);
-                }
+            var basicTotal = 0;
+            var nonBasicTotal = 0;
+            var tenthTotal = 0;
+            var hundredTotal = 0;
+            var finaleTotal = 0;
+            var forEachBasic = 0;
+            var extra = 0;
 
-
-                //sum of the rest
-                for(int l = 0; l < 10; l++)
-                {
-                    for(int i = 0; i < 8; i++)
-                    {
-                        doublesTotal += Int32.Parse(arrayOfDoubles[i]);
-                    }
-                }
-                doublesTotal += 8*36;
+            for(var b = 0; b < basicNums.Count; b++)
+            {
+                forEachBasic += basicNums[b];
             }
 
-            counter += singlesTotal + patternLessTotal + doublesTotal + 7*9;
-            Console.WriteLine("Problem 17:\n" + counter);
+            for(var n = 0; n < nonBaiscNums.Count; n++)
+            {
+                nonBasicTotal += nonBaiscNums[n];
+            }
+
+            for(int c = 0; c < 10; c++)
+            {
+                for(var t = 0; t < tenthNums.Count; t++)
+                {
+                    tenthTotal += tenthNums[t];
+                }
+            }
+
+            for(int c = 0; c < 8; c++)
+            {
+                for(var b = 0; b < basicNums.Count; b++)
+                {
+                    tenthTotal += basicNums[b];
+                }
+            }
+
+            extra += ( forEachBasic + nonBasicTotal + tenthTotal + hundredTotal );
+
+            for(int eachHundred = 0; eachHundred < 9; eachHundred++)
+            {
+                finaleTotal += ( forEachBasic + nonBasicTotal + tenthTotal + hundredTotal );
+            }
+
+            for(int hundredand = 0; hundredand < 891; hundredand++)
+            {
+                hundredTotal += ( hundred + and );
+            }
+
+            for(int hundreds = 0; hundreds < 9; hundreds++)
+            {
+                hundredTotal += hundred;
+            }
+
+            for(int eachBasic = 0; eachBasic < 100; eachBasic++)
+            {
+                basicTotal += forEachBasic;
+            }
+
+            finaleTotal += ( basicTotal + hundredTotal + extra + onethousand );
+
+            Console.WriteLine("Problem 17:\nThe sum of all the letters between 1 to 1000 is " + finaleTotal);
         }
     }
 }
---
+++ Project-Euler/Project-Euler/Problem24.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler
+{
+    public class Problem24
+    {
+        public List<int> PermutationList = new List<int>();
+
+
+        public void ReturnResult()
+        {
+
+            for(int i = 0; i <= 9; i++)
+            {
+                PermutationList.Add(i);
+            }
+
+            for(var count = 0; count < 1000000; count++)
+            {
+                
+
+
+            }
+        }
+    }
+}
